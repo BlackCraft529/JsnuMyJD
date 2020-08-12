df9910e1ab851303c3d9902186a540735846de23
YuuJinnA <mr.hu.clear@gmail.com>
2020-08-12 18:43:03 +0800

    2020.8.12个人信息页面完善

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -11,6 +11,17 @@
     }]).controller('searchCtrl',['$scope','$http','$routeParams',function ($scope,$http,$routeParams) {
         $scope.search_key=$routeParams.search_key;
         console.log($scope.search_key);
-
+        $http({
+            url:'/getSomething',//验证表单的接口
+            method:'post',
+            data:{
+                "search_key" : $scope.search_key,
+            },
+            headers:{'Content-Type':'application/json;charset=UTF-8'}, //将其变为 json 参数形式
+        }).then(function successCallback(data) {
+            console.log(data);
+        }, function errorCallback(response) {
+            alert("error!\n"+"error message:"+response);
+        });
     }]);
 })(angular);
