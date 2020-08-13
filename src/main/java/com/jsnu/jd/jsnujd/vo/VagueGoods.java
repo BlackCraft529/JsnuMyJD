e315d77e8c778e623b500d808f0426703eabb4e8
BlackCrafts <13160292800@163.com>
2020-08-13 10:51:33 +0800

    GoodsVo更新;

--- src/main/java/com/jsnu/jd/jsnujd/vo/VagueGoods.java
+++ src/main/java/com/jsnu/jd/jsnujd/vo/VagueGoods.java
@@ -1,6 +1,7 @@
 package com.jsnu.jd.jsnujd.vo;
 
 import com.jsnu.jd.jsnujd.pojo.Goods;
+import com.jsnu.jd.jsnujd.utils.StaticServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,4 +17,14 @@ import java.util.List;
 public class VagueGoods {
     private List<Goods> goodsList;
     private boolean result;
+
+    public void setGoodsList(List<Goods> goodsList){
+        String goodsPublisher="";
+        for(Goods goods:goodsList){
+            goodsPublisher = StaticServiceImpl.getUserService()
+                    .selectUserByUserId(goods.getPublisher()).getName();
+            goods.setPublisher(goodsPublisher);
+            this.goodsList.add(goods);
+        }
+    }
 }
